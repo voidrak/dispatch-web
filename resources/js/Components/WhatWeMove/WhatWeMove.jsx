2ce469692a5e754df3750e40de4fa9e40d838c20
RohaTech <raklovetocode@gmail.com>
2024-11-04 18:15:33 +0300

    "Updated WhatWeMove component with new image assets and modified CSS styles"

--- resources/js/Components/WhatWeMove/WhatWeMove.jsx
+++ resources/js/Components/WhatWeMove/WhatWeMove.jsx
@@ -3,7 +3,7 @@ import RevealXLeft from "../../utils/RevealXLeft";
 
 export default function WhatWeMove() {
   return (
-    <div className="flex flex-col bg-bg-light px-1 py-8">
+    <div className="flex flex-col bg-bg-light px-1">
       <RevealXLeft>
         <div className="mx-auto max-w-[550px] text-center sm:mx-0 sm:ml-8 sm:text-left lg:max-w-[650px] xl:px-16">
           <h1 className="text-nowrap text-4xl font-extrabold sm:text-6xl lg:text-8xl 2xl:text-9xl">
@@ -16,39 +16,51 @@ export default function WhatWeMove() {
           </p>
         </div>
       </RevealXLeft>
-      <div className="mx-auto mt-16 grid max-w-[437x] grid-cols-2 gap-1 sm:grid-cols-3 lg:px-4 xl:max-w-[1280px] xl:gap-3 xl:px-16 2xl:min-w-[1536px]">
+      <div className="mx-auto grid max-w-[437x] grid-cols-2 gap-1 pt-16 min-[450px]:px-2 sm:grid-cols-3 md:px-6 lg:px-8 xl:max-w-[1280px] xl:gap-3 xl:px-16 2xl:min-w-[1536px]">
         <img
           className="duration-300 hover:z-10 hover:translate-x-4 hover:translate-y-2 hover:scale-105 xl:w-[100%]"
           src=" images/what-we-move-top-left.webp"
-          alt="track-image-1"
+          alt="sprinter van"
         />
         <img
           className="duration-300 hover:z-10 hover:translate-x-4 hover:translate-y-2 hover:scale-105 xl:w-[100%]"
           src="images/what-we-move-top-center.webp"
-          alt="track-image-2"
+          alt="removable gooseneck"
         />
         <img
-          className="hidden duration-300 hover:z-10 hover:translate-x-4 hover:translate-y-2 hover:scale-105 sm:block xl:w-[100%]"
+          className="-translate-y-[31%] duration-300 hover:z-10 hover:translate-x-4 hover:translate-y-2 hover:scale-105 sm:translate-y-0 xl:w-[100%]"
           src="images/what-we-move-top-right.webp"
-          alt="track-image-3"
+          alt="dry van"
         />
         <img
-          className="-translate-y-[30%] duration-300 hover:z-10 hover:-translate-y-[31%] hover:translate-x-4 hover:scale-105 sm:-translate-y-[40%] xl:w-[100%]"
+          className="translate-y-[10%] duration-300 hover:z-10 hover:-translate-y-[43%] hover:translate-x-4 hover:scale-105 sm:-translate-y-[41%] xl:w-[100%]"
+          src="images/what-we-move-center-left.webp"
+          alt="semi track"
+        />
+        <img
+          className="-translate-y-[50%] duration-300 hover:z-10 hover:-translate-y-[15%] hover:translate-x-4 hover:scale-105 sm:-translate-y-[10%] xl:w-[100%]"
+          src="images/what-we-move-center-center.webp"
+          alt="flat bed"
+        />
+        <div className="bg-transparent"></div>
+        <img
+          className="-translate-y-[50%] duration-300 hover:z-10 hover:-translate-y-[67%] hover:translate-x-4 hover:scale-105 sm:-translate-y-[65%] xl:w-[100%]"
           src="images/what-we-move-bottom-left.webp"
-          alt="track-image-4"
+          alt="straight box truck"
         />
+        <div className="bg-transparent sm:hidden sm:bg-red-500"></div>
         <img
-          className="duration-300 hover:z-10 hover:translate-x-4 hover:translate-y-2 hover:scale-105 sm:-translate-y-[18%] xl:w-[100%] xl:-translate-y-[10%]"
+          className="mb-10 -translate-y-[50%] duration-300 hover:z-10 hover:-translate-y-[68%] hover:translate-x-4 hover:scale-105 sm:-translate-y-[65%] xl:w-[100%]"
           src="images/what-we-move-bottom-center.webp"
-          alt="track-image-5"
+          alt="reefer"
         />
         <img
-          className="hidden duration-300 hover:z-10 hover:translate-x-4 hover:translate-y-2 hover:scale-105 sm:block xl:w-[100%]"
-          src="images/what-we-move-top-left.webp"
-          alt="track-image-6"
+          className="-translate-y-[90%] duration-300 hover:z-10 hover:-translate-y-[85%] hover:translate-x-4 hover:scale-105 sm:-translate-y-[83%] xl:w-[100%]"
+          src="images/what-we-move-bottom-right.webp"
+          alt="step deck"
         />
       </div>
-      <button className="mx-auto -translate-y-4 rounded-md bg-black px-8 py-2 text-white duration-200 hover:scale-105 lg:-translate-y-16 lg:px-16 lg:py-4 xl:-translate-y-24 2xl:-translate-y-32">
+      <button className="mx-auto -translate-y-[150px] rounded-md bg-black px-8 py-2 text-white duration-200 hover:scale-105 md:-translate-y-[250px] lg:-translate-y-[350px] lg:px-16 lg:py-4 xl:-translate-y-[450px]">
         <a href="#contact" className="">
           MORE
         </a>
